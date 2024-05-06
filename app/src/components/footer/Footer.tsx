0631210792111ea5019b42e5bea3e56e76200472
WictorHiago <wictor.hsc@gmail.com>
2024-05-06 05:31:05 -0400

    styles

--- app/src/components/footer/Footer.tsx
+++ app/src/components/footer/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <>
-      <footer className="flex bg-white rounded-lg shadow m-4 bg-black h-[100px]">
+      <footer className="flex rounded-lg shadow m-4 bg-black h-[100px]">
         <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
             © 2024{" "}
